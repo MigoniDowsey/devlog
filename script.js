c7d08010581be7dcd2c68907ec6bed368e396ef0
MigoniDowsey <mdowsey@icstars.org>
2025-09-19 10:32:13 -0500

    Add files via upload
    Initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+let username ="Migoni";
+let number ="6";
+console.log(number);
+let numbers = 6;
+let x = number + numbers;
+console.log(x)
+let one = 1;
+let z =number + one;
+console.log(z);
+
+let oneword = "Migoni";
+let twoword= "Dowsey";
+let cto =oneword + " " + twoword;
+console.log (cto);
+let a =22;
+function add (a,b) {
+let a = 22;
+let b =10;
+let c= a+b
+console.log(c);
+}
+add (33,22);
+console.log (sum)
